ad9926edeb0abba52f0aeea949725ab54a95ff06
kimjaechan <penguin_03@naver.com>
2026-01-29 15:39:49 +0900

    feat: app모듈에 AuthModule, UserModule 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -9,6 +11,8 @@ import { AppService } from './app.service';
       envFilePath: `.env.${process.env.NODE_ENV}`,
       isGlobal: true,
     }),
+    AuthModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
