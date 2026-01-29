aae82c549fbd46a5a089534d9b8bb55df6886db3
kimjaechan <penguin_03@naver.com>
2026-01-29 15:33:22 +0900

    feat: prisma 모듈 생성 및 설정

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import { PrismaClient } from 'generated/prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    const adapter = new PrismaMariaDb({
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    });
+
+    super({
+      adapter,
+      log: ['query', 'info', 'warn', 'error'],
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
