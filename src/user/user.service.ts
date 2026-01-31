d1f785e3dd345e3719e4009876205d5f832ee2ea
kimjaechan <penguin_03@naver.com>
2026-01-31 14:17:27 +0900

    refactor: User 모듈 트랜잭션 적용
    - src/user/user.module.ts: PrismaModule import 제거 (Global이므로 불필요)
    - src/user/user.repository.ts
      - txHost.tx 사용하도록 변경
      - update(): 사용자 정보 업데이트
    - src/user/user.service.ts
      - update(): 이메일로 사용자 조회 후 업데이트

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,18 +1,27 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from 'src/types/types';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CreateUserDto, UpdateUserDto } from 'src/types/types';
 import { UserRepository } from './user.repository';
 
 @Injectable()
 export class UserService {
   constructor(private readonly userRepository: UserRepository) {}
 
-  findOne({ unique }: { unique: number | string }) {
+  async findOne({ unique }: { unique: number | string }) {
     return this.userRepository.findByUnique({
       unique,
     });
   }
 
-  create(data: CreateUserDto) {
+  async create(data: CreateUserDto) {
     return this.userRepository.create(data);
+  }
+
+  async update(email: string, update: UpdateUserDto) {
+    const user = await this.userRepository.findByUnique({
+      unique: email,
+    });
+    if (!user) throw new BadRequestException('이메일을 다시 확인해주세요.');
+
+    return this.userRepository.update(user, update);
   }
 }
